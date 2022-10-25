5c84f8160d1231e7648bb9159378e413555b8b89
Anirudha Gupta <anirudhagupta01@gmail.com>
2022-10-25 09:20:09 +0530

    Add project files.

---
+++ Controllers/ProxyController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.IIS.Core;
+
+namespace Proxy.Controllers
+{
+    [Route("api/[controller]")]
+    [EnableCors("AllowOrigin")]
+    [ApiController]
+    public class ProxyController : ControllerBase
+    {
+        private readonly HttpClient client;
+        private readonly IConfiguration configuration;
+
+        public ProxyController(HttpClient client, IConfiguration config)
+        {
+            this.client = client;
+            this.configuration = config;
+        }
+
+        [HttpGet]
+        public async Task<string> Get([FromQuery] string url)
+        {
+            string dir = configuration["dir"];
+            string fileName = url.SanitizeName();
+            var path = Path.Combine(dir, fileName);
+            try
+            {
+                if (System.IO.File.Exists(path))
+                {
+                    return await System.IO.File.ReadAllTextAsync(path);
+                }
+                else
+                {
+                    string response = await this.client.GetStringAsync(url);
+                    System.IO.File.WriteAllText(path, response);
+                    return response;
+                }
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+            
+        }
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,14 @@
+﻿namespace Proxy
+{
+    public static class Utils
+    {
+        public static string SanitizeName(this string fileName)
+        {
+            foreach (char c in System.IO.Path.GetInvalidFileNameChars())
+            {
+                fileName = fileName.Replace(c, '_');
+            }
+            return fileName;
+        }
+    }
+}
