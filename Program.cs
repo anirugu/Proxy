c96e979d93490b9349bdad082ae821ee754c4672
Anirudha Gupta <35772421+anirugu@users.noreply.github.com>
2022-11-10 11:43:39 +0530

    enable swagger in dev and prod both

--- Program.cs
+++ Program.cs
@@ -21,11 +21,9 @@ namespace Proxy
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
