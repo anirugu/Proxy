91dcb957bbfe6343db94f86fec24f563fddafc64
Anirudha Gupta <35772421+anirugu@users.noreply.github.com>
2022-12-08 08:01:20 +0530

    fix the caching logic to cache for next 5 day

--- Utils.cs
+++ Utils.cs
@@ -16,14 +16,18 @@
             var dir = System.IO.Path.Combine(System.IO.Path.GetTempPath(), GetDirectory());
             if (!System.IO.Directory.Exists(dir))
                 System.IO.Directory.CreateDirectory(dir);
-           return System.IO.Path.Combine(dir, url.SanitizeName());
+            return System.IO.Path.Combine(dir, url.SanitizeName());
         }
-
+        
+        /// <summary>
+        /// Update every 5 day
+        /// </summary>
+        /// <returns></returns>
         private static string GetDirectory()
         {
             var date = DateTime.Now;
             int month = date.Month;
-            int h = month > 15 ? 1 : 0;
+            int h = date.Day % 5;
             return $"{month}{h}";
         }
     }
